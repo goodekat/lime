84b39988a4c7a161f8a4004bc88d0ddf92eff82d
Katherine Goode <kgoode@iastate.edu>
2019-11-18 12:12:58 -0600

    fixed another bug

--- R/a_dataframe.R
+++ R/a_dataframe.R
@@ -184,7 +184,7 @@ explain.data.frame <- function(x, explainer, labels = NULL, n_labels = NULL,
   })
   res <- do.call(rbind, res)
   res <- res[, c('model_type', 'case', 'label', 'label_prob', 'model_r2', 'model_intercept', 'model_prediction', 'feature', 'feature_value', 'feature_weight', 'feature_desc', 'data', 'prediction', 
-                 'perms_raw', 'perms_numerified', 'perm_pred', 'weights')]
+                 'perms_raw', 'perms_numerified', 'perm_pred', 'weights', 'fs', 'hw', 'lp', 'tree')]
   if (m_type == 'regression') {
     res$label <- NULL
     res$label_prob <- NULL
